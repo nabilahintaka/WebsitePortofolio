7a2751a2cec1327fce735a3c82ef6eadd74d4ff7
nabilahintaka <nabilahintaka@gmail.com>
2025-01-08 00:25:40 +0700

    change color

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -28,7 +28,7 @@ function AboutCard() {
             </li>
           </ul>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
+          <p style={{ color: "rgb(130, 163, 235)" }}>
             "Experience is the name everyone gives to their mistakes"{" "}
           </p>
           <footer className="blockquote-footer">Oscar Wilde</footer>
