9a8cc938fd51926c9ff4aff5fa9ef64a51c8238d
nabilahintaka <nabilahintaka@gmail.com>
2025-06-25 12:56:03 +0700

    add new button

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -5,36 +5,66 @@ import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
 
 function ProjectCards(props) {
+
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
+
+  const purpleStyle = {
+    backgroundColor: "#c770f0",
+    color: "white",
+    border: "none",
+  };
+
   return (
-    <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>
-          {props.description}
-        </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
-        {"\n"}
-        {"\n"}
-
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
-
-        {!props.isBlog && props.demoLink && (
+    <>
+      <Card className="project-card-view">
+        <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+        <Card.Body>
+          <Card.Title>{props.title}</Card.Title>
+          <Card.Text style={{ textAlign: "justify" }}>
+            {props.description}
+          </Card.Text>
           <Button
             variant="primary"
-            href={props.demoLink}
-            target="_blank"
-            style={{ marginLeft: "10px" }}
+            onClick={handleShow}
+            style={{ ...purpleStyle, marginRight: "10px" }}
           >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
+            View
           </Button>
-        )}
-      </Card.Body>
-    </Card>
+          <Button variant="primary" href={props.ghLink} target="_blank">
+            <BsGithub /> &nbsp;
+            {props.isBlog ? "Blog" : "GitHub"}
+          </Button>
+          {"\n"}
+          {"\n"}
+
+          {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
+
+          {!props.isBlog && props.demoLink && (
+            <Button
+              variant="primary"
+              href={props.demoLink}
+              target="_blank"
+              style={{ marginLeft: "10px" }}
+            >
+              <CgWebsite /> &nbsp;
+              {"Demo"}
+            </Button>
+          )}
+        </Card.Body>
+      </Card>
+
+      <Modal show={showModal} onHide={handleClose} size="lg" centered>
+      <Modal.Header closeButton style={purpleStyle}>
+        <Modal.Title>{props.title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body style={{ backgroundColor: "#c770f0", padding: 0 }}>
+        <img src={props.imgPath} alt={props.title} style={{ width: '100%', height: 'auto' }} />
+      </Modal.Body>
+    </Modal>
+  </>
   );
 }
 export default ProjectCards;
