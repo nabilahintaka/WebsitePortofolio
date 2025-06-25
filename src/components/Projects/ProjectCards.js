8ed32734d8929da07dc4885adc14c46fdb9d6576
nabilahintaka <nabilahintaka@gmail.com>
2025-06-25 12:58:37 +0700

    add new button

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -3,6 +3,7 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
+import Modal from "react-bootstrap/Modal";
 
 function ProjectCards(props) {
 
